0883a6597c9599d410b9bf7fb06fd22ca841fe0a
Kevin Maciuba <kmaciub1@jhu.edu>
2015-08-25 11:24:39 -0400

    filter lines 10 through 15

--- day2/filter.py
+++ day2/filter.py
@@ -4,8 +4,15 @@ filename= "/Users/cmdb/qbb2015/stringtie/SRR072893/t_data.ctab"
 
 f = open( filename )
 
-for line in f:
-    fields = line.split()
-    if "tRNA" in fields[9]:
-        print line,
+
+for line_count, data in enumerate( f ):
+    if line_count <= 10:
+        pass
+    elif line_count <= 15:
+        print data,
+    else:
+        break
+
+        
+        
 
