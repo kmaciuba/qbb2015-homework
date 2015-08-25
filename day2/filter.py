bb08f3804230edc8ee44dcfb2e7b8c0247c81f00
Kevin Maciuba <kmaciub1@jhu.edu>
2015-08-25 11:35:03 -0400

    gene names and values

--- day2/filter.py
+++ day2/filter.py
@@ -4,15 +4,18 @@ filename= "/Users/cmdb/qbb2015/stringtie/SRR072893/t_data.ctab"
 
 f = open( filename )
 
+name_counts = {}
 
 for line_count, data in enumerate( f ):
-    if line_count <= 10:
-        pass
-    elif line_count <= 15:
-        print data,
-    else:
-        break
+    fields = data.split()
+    gene_name = fields [9]
+    if gene_name not in name_counts:
+        name_counts [ gene_name ] = 1
+    else: 
+        name_counts [ gene_name ] += 1
 
-        
+#Iterate key, value pairs from name counts dictionary
+for key, value in name_counts.iteritems():
+    print key, value
         
 
