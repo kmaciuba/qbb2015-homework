958d274a59dfbdcf8f7ba372d5b900183609bf6a
Kevin Maciuba <kmaciub1@jhu.edu>
2015-08-26 09:41:40 -0400

    changed to get rid of 0s

--- day2/day2homework/question4.py
+++ day2/day2homework/question4.py
@@ -7,15 +7,28 @@ annotation="/USERS/cmdb/qbb2015/stringtie/SRR072893/t_data.ctab"
 
 df = pd.read_table(annotation)
 
-df2= df["FPKM"]
 
-top= df2[0:11572]
-middle= df2[11572:23144]
-bottom= df2[23144:34718]
+
+df2= df["FPKM"] >0
+
+df3= df["FPKM"][df2]
+
+#print df3
+        
+
+
+#top= df2[0:11572]
+#middle= df2[11572:23144]
+#bottom= df2[23144:34718]
+
+top= df3[0:3183]
+middle= df3[3183:6366]
+bottom= df3[6366:9549]
+
 
 plt.figure()
 plt.title("FPKM")
-plt.boxplot([top,middle,bottom])
+plt.boxplot([top, middle, bottom])
 plt.xlabel("Location")
 plt.ylabel("FPKM")
 plt.savefig("FPKM.png")
