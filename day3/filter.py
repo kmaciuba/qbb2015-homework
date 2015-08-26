42556f2431262ea11d6eb5e348121b213c3643cb
Kevin Maciuba <kmaciub1@jhu.edu>
2015-08-26 10:19:46 -0400

    stdin argv

---
+++ day3/filter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+#filename= "/Users/cmdb/qbb2015/stringtie/SRR072893/t_data.ctab"
+
+import sys
+
+#Open using arguments:
+
+#print sys.argv
+#filename= sys.argv[1]
+
+
+#f = open( filename )
+
+f = sys.stdin
+
+name_counts = {}
+
+for line_count, data in enumerate( f ):
+    fields = data.split()
+    gene_name = fields [9]
+    if gene_name not in name_counts:
+        name_counts [ gene_name ] = 1
+    else: 
+        name_counts [ gene_name ] += 1
+
+#Iterate key, value pairs from name counts dictionary
+for key, value in name_counts.iteritems():
+    print key, value
+        
+
